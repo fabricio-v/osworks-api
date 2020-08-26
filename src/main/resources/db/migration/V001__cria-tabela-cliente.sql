0bbced6bf2a2eedd4df29a7d731e474a43b17688
Fabricio Ventura <fabricio.ventura@lcsistemas.com.br>
2020-08-26 14:18:02 -0300

    Adicionei a dependencia do Flyway

---
+++ src/main/resources/db/migration/V001__cria-tabela-cliente.sql
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Fabricio
+ * Created: 26/08/2020
+ */
+
